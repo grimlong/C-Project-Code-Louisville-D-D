40793047c46e268375b22b8ec8365d13a8f86389
grimlong <jasonmillers197@gmail.com>
2020-10-19 14:39:40 -0400

    Added Class Attributes

--- Program.cs
+++ Program.cs
@@ -21,11 +21,34 @@ namespace C__Project
 		Console.WriteLine("Welcome, " + cName);
 		Console.WriteLine("What is your class (Fighter, Wizard, Rogue, etc.)");
 		string cClass = Console.ReadLine();
+
+		//roll stats and HP than print name, class, and stats.
+
 		Console.WriteLine(cName + " " + "the " + cClass);
 
 	
 	}
-
+  class Attributes {
+	public int str;
+	public int strMod;
+	        
+	public int dex;
+	public int dexMod;
+	        
+	public int con;
+	public int conMod;
+	        
+	public int itl;
+	public int itlMod;
+	        
+	public int wis;
+	public int wisMod;
+	        
+	public int cha;
+	public int chaMod;
+	        
+	public int HP;  
+  }
 	//roll stats
 
 	//level up
