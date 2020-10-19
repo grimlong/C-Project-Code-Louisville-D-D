cad8ce0a23d126a998004bd83b6d348fe359b981
grimlong <jasonmillers197@gmail.com>
2020-10-19 11:19:18 -0400

    initial Commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace C__Project
+{
+
+	
+	//choose class
+
+	//determin HP based on class
+
+	//roll stats
+	public class Attributes 
+	{
+		public int Str;
+		public int Dex;
+		public int Con;
+		public int Int;
+		public int Wis;
+		public int Cha;
+
+		public int Hp;
+
+		public Random random_num_gen;
+
+		public Character()
+	}
+	
+
+	//level up
+}
